edc624d8e5ffd53c81aa313c9fed816aabd55dd7
Deathbydragonfire <hanna.kessler10@gmail.com>
2018-10-10 22:07:58 -0500

    combined open cv and ui (still not working)

--- Prototype/app/src/main/java/com/blackjack/woods/prototype/MainActivity.java
+++ Prototype/app/src/main/java/com/blackjack/woods/prototype/MainActivity.java
@@ -26,40 +26,80 @@ import android.util.Size;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.Surface;
+import android.view.SurfaceView;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 import java.util.Collections;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener , CameraBridgeViewBase.CvCameraViewListener2{
 
 
-    //defining the variables
+    //defining the menu variables
     private DrawerLayout mDrawerLayout;
-
+    //open cv variables
+    CameraBridgeViewBase base;
+    BaseLoaderCallback callback;
+    Mat mat1, mat2, mat3;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //menu stuff
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            //declares width and heights
+            int width = displayMetrics.widthPixels;
+            int hieght = displayMetrics.heightPixels;
 
-        //declares width and heights
-        int width = displayMetrics.widthPixels;
-        int hieght = displayMetrics.heightPixels;
-
-        //set the layout to fullscreen
-        getWindow().setLayout(width,hieght);
+            //set the layout to fullscreen
+            getWindow().setLayout(width,hieght);
 
 
-        //defining the menu
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
-        setNavigationViewListener();
+            //defining the menu
+            mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+            setNavigationViewListener();
+         //end menu stuff
+
+        //open cv stuff
+        if (OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "OpenCV ran successfully", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Nope, it don't work", Toast.LENGTH_SHORT).show();
+        }
+        base = (JavaCameraView)findViewById(R.id.myCameraView);
+        base.setVisibility(SurfaceView.VISIBLE);
+        base.setCvCameraViewListener(this);
+        callback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+
+                switch (status) {
+                    case BaseLoaderCallback.SUCCESS:
+                        base.enableView();
+                        break;
+
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
 
     }
 
@@ -114,6 +154,55 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //close navigation drawer
         mDrawerLayout.closeDrawer(Gravity.START);
         return true;
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame theFrame) {
+        mat1 = theFrame.rgba();
+        //Core.transpose(mat1, mat2);
+        //Imgproc.resize(mat2, mat3, mat3.size(), 0, 0, 0);
+        //Core.flip(mat3, mat1, 1);
+        return mat1;
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mat1.release();
+        mat2.release();
+        mat3.release();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mat1 = new Mat(width, height, CvType.CV_8UC4);
+        mat2 = new Mat(width, height, CvType.CV_8UC4);
+        mat3 = new Mat(width, height, CvType.CV_8UC4);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (base != null) {
+            base.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "Nope, it don't work again", Toast.LENGTH_SHORT).show();
+        } else {
+            callback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (base != null) {
+            base.disableView();
+        }
     }
 
 }
