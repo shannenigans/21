31f073bdfff38bee36c8266ae05fc4e090a1e2a8
Aiden Woodside <aaw170001@utdallas.edu>
2018-10-10 20:47:20 -0500

    More Menu functionality

--- Prototype/app/src/main/java/com/blackjack/woods/prototype/MainActivity.java
+++ Prototype/app/src/main/java/com/blackjack/woods/prototype/MainActivity.java
@@ -1,6 +1,7 @@
 package com.blackjack.woods.prototype;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
@@ -17,23 +18,24 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Size;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 
 import java.util.Collections;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
 
-    TextureView textureView;
-private DrawerLayout mDrawerLayout;
-Size size;
-
+    //defining the variables
+    private DrawerLayout mDrawerLayout;
 
 
 
@@ -42,17 +44,76 @@ Size size;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        //declares width and heights
+        int width = displayMetrics.widthPixels;
+        int hieght = displayMetrics.heightPixels;
+
+        //set the layout to fullscreen
+        getWindow().setLayout(width,hieght);
+
+
+        //defining the menu
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        setNavigationViewListener();
 
     }
 
 
 
-    public void buttonOnClick(View v){
+    public void menuButtonOnClick(View v){
+        //opens up the sliding menu
         mDrawerLayout.openDrawer(Gravity.START);
+
+
+
+    }
+
+    public void pictureButtonOnClick(View v){
+        //insert functionality
+    }
+
+    public void readTableButtonOnClick(View v){
+        //insert functionality
+    }
+
+    //checks to see if the navigation is being used
+    private void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationLayout);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
 
+    //this function runs when the menu is opened and items are clicked
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+
+            case R.id.betUnits: {
+                startActivity(new Intent(MainActivity.this,Pop.class));
+                break;
+            }
+            case R.id.cardsInShoe:{
+
+                break;
+            }
+            case R.id.settings:{
+
+                break;
+            }
+            case R.id.resetGame:{
+
+                break;
+            }
+        }
+        //close navigation drawer
+        mDrawerLayout.closeDrawer(Gravity.START);
+        return true;
+    }
 
 }
