cb2c8aef06066e2cfe558f1e861106b19ed084da
Aiden Woodside <aaw170001@utdallas.edu>
2018-10-24 21:11:47 -0500

    Added permission functionality to the app. App prompts the user for permissions and will not run without them.

--- Prototype/app/src/main/java/com/blackjack/woods/prototype/MainActivity.java
+++ Prototype/app/src/main/java/com/blackjack/woods/prototype/MainActivity.java
@@ -19,6 +19,8 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
@@ -53,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     BaseLoaderCallback callback;
     Mat mat1, mat2, mat3;
 
+    private static final int CAMERA_REQUEST = 1888;
 
 
     @Override
@@ -61,6 +64,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get camera permissions
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST);
+
 
         //menu stuff
             DisplayMetrics displayMetrics = new DisplayMetrics();
